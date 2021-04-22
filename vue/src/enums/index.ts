3a29f341a291b10f83b474eb8d755ca86c068afe
John Campbell <johncampbell@geospatialweb.ca>
2021-04-22 16:19:56 -0400

    create enum directory

---
+++ vue/src/enums/index.ts
@@ -0,0 +1,17 @@
+export enum StoreActions {
+  setMapSettings = 'SET_MAP_SETTINGS'
+}
+
+export enum StoreGetters {
+  getMapSettings = 'GET_MAP_SETTINGS'
+}
+
+export enum StoreMutations {
+  setMapSettings = 'SET_MAP_SETTINGS'
+}
+
+export enum Urls {
+  GEOJSON_ENDPOINT = '/api/geojson',
+  HEATMAP_DATA_URL = 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv',
+  MAPBOX_ACCESS_TOKEN_ENDPOINT = '/api/mapbox-access-token'
+}
