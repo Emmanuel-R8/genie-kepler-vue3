15d299bf5ed39938aa99d817c334199d5760a417
John Campbell <johncampbell@geospatialweb.ca>
2021-04-22 16:19:34 -0400

    create layers config

---
+++ vue/src/config/layers.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 'biosphere',
+    fields: 'name, description, geom'
+  },
+  {
+    id: 'office',
+    fields: 'name, description, geom'
+  },
+  {
+    id: 'places',
+    fields: 'name, description, geom'
+  },
+  {
+    id: 'trails',
+    fields: 'name, description, lat, lng, geom'
+  }
+]
