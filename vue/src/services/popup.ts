8ddc9ae94f6f33f4702fca2e9d1bb28b272bbe87
John Campbell <johncampbell@geospatialweb.ca>
2021-04-25 02:29:34 -0400

    create popup service

---
+++ vue/src/services/popup.ts
@@ -0,0 +1,48 @@
+import { Feature, Point } from 'geojson'
+import { LngLatLike, MapLayerMouseEvent, Popup } from 'mapbox-gl'
+import { Container, Service } from 'typedi'
+
+import { MapboxService } from '@/services'
+
+@Service()
+export default class PopupService {
+  constructor(private _mapboxService: MapboxService, private _popup: Popup) {
+    this._mapboxService = Container.get(MapboxService)
+    this._popup = new Popup({
+      closeButton: false,
+      offset: 5
+    })
+  }
+
+  addLayerPopup(evt: MapLayerMouseEvent): void {
+    if (evt.features && evt.features.length && evt.features[0].properties) {
+      this._popup
+        .setHTML(
+          `<div class="bold">${evt.features[0].properties.name}</div>
+           <div>${evt.features[0].properties.description}</div>`
+        )
+        .setLngLat(evt.lngLat)
+
+      this._mapboxService.addToMap(this._popup)
+    }
+  }
+
+  addMarkerPopup(layer: string, feature: Feature): void {
+    if (feature?.properties) {
+      this._popup.setHTML(
+        `<div class="bold">${feature.properties.name}</div>
+         <div>${feature.properties.description}</div>`
+      )
+
+      layer === 'trails'
+        ? this._popup.setLngLat([feature.properties.lng, feature.properties.lat] as LngLatLike)
+        : this._popup.setLngLat((feature.geometry as Point).coordinates as LngLatLike)
+
+      this._mapboxService.addToMap(this._popup)
+    }
+  }
+
+  removePopup(): void {
+    this._popup.remove()
+  }
+}
