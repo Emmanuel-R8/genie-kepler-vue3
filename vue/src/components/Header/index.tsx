df5de944cbe9e1f32d4a73697032978082132dd0
John Campbell <johncampbell@geospatialweb.ca>
2021-05-06 14:33:22 -0400

    code refactoring

--- vue/src/components/Header/index.tsx
+++ vue/src/components/Header/index.tsx
@@ -6,7 +6,7 @@ const html = (): JSX.Element => (
   <div class={scss.header}>
     <img src="assets/logo.png" alt="Geospatial Web" />
     <div class={scss.name}>Geospatial Web</div>
-    <div class={scss.title}>Go | Vue 3 TSX | Vuex | Mapbox GL | Deck.gl | PostGIS 3 | Docker</div>
+    <div class={scss.title}>Go | Vue 3 Composition API & TSX | Mapbox GL | Deck.gl | PostGIS 3</div>
     <a
       href="https://gitlab.com/geospatialweb/go-vue3-tsx"
       rel="noopener noreferrer"
--- vue/src/e2e/main.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('Main', () => {
-  it('should display title text', () => {
-    cy.visit('/')
-    cy.contains('div.title', 'Go | Vue 3 TSX | Vuex | Mapbox GL | Deck.gl | PostGIS 3 | Docker')
-  })
-})
