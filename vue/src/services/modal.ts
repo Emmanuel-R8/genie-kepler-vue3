35be030839764d074168b7056b27488a31abf625
John Campbell <johncampbell@geospatialweb.ca>
2021-05-16 20:31:20 -0400

    add modal service

---
+++ vue/src/services/modal.ts
@@ -0,0 +1,29 @@
+import cloneDeep from 'lodash/cloneDeep'
+import { Container, Service } from 'typedi'
+
+import { IModal } from '@/interfaces'
+import { StoreService } from '@/services'
+
+@Service()
+export default class ModalService {
+  constructor(private _modalState: IModal, private _storeService: StoreService) {
+    this._storeService = Container.get(StoreService)
+  }
+
+  hideModal = (timeout?: number): void => {
+    this._modalState = this.getModalState()
+    this._modalState.active && this.setModalState(timeout)
+  }
+
+  showModal = (timeout?: number): void => {
+    this._modalState = this.getModalState()
+    !this._modalState.active && this.setModalState(timeout)
+  }
+
+  private getModalState = (): IModal => cloneDeep(this._storeService.getModalState())
+
+  private setModalState = (timeout?: number): number | void =>
+    timeout
+      ? setTimeout((): void => this._storeService.setModalState(), timeout)
+      : this._storeService.setModalState()
+}
