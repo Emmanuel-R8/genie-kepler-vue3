994855699641e1d23e92b2cbf8eb299448d15520
John Campbell <johncampbell@geospatialweb.ca>
2021-06-04 22:10:29 -0400

    refactor MarkerService

--- vue/src/services/marker.ts
+++ vue/src/services/marker.ts
@@ -22,11 +22,10 @@ export default class MarkerService {
   }
 
   setMarkers(id: string, features: Feature[]): void {
-    const marker: Marker[] = []
-    for (const feature of features) {
-      marker.push(this.createMarker(id, feature))
-    }
-    this._markers.push(marker)
+    const markers: Marker[] = features.map(
+      (feature: Feature): Marker => this.createMarker(id, feature)
+    )
+    this._markers = [...this._markers, markers]
     this._markersHash[id] = this._markers.length - 1
   }
 
@@ -34,14 +33,14 @@ export default class MarkerService {
     for (const markers of this._markers) {
       for (const marker of markers) {
         const el: IHTMLMarkerElement = <IHTMLMarkerElement>marker.getElement()
-        if (!el.hidden && !el.visible) {
+        if (!el.isActive && !el.isHidden) {
           break
         }
 
-        el.hidden = !el.hidden
-        el.hidden && marker.remove()
-        el.visible = !el.visible
-        el.visible && marker.addTo(this._mapboxService.map)
+        el.isActive = !el.isActive
+        el.isActive && marker.addTo(this._mapboxService.map)
+        el.isHidden = !el.isHidden
+        el.isHidden && marker.remove()
       }
     }
   }
@@ -49,29 +48,14 @@ export default class MarkerService {
   toggleMarkers(layerElement: ILayerElement): void {
     for (const marker of this._markers[this._markersHash[layerElement as keyof ILayerElement]]) {
       const el: IHTMLMarkerElement = <IHTMLMarkerElement>marker.getElement()
-      el.visible = !el.visible
-      el.visible ? marker.addTo(this._mapboxService.map) : marker.remove()
+      el.isActive = !el.isActive
+      el.isActive ? marker.addTo(this._mapboxService.map) : marker.remove()
     }
-  }
-  /* create individual html marker elements & add mouse event handlers */
-  private createHTMLMarkerElement(id: string, feature: Feature): IHTMLMarkerElement {
-    const el: IHTMLMarkerElement = <IHTMLMarkerElement>document.createElement('div')
-    el.className = `${id}-marker`
-    el.hidden = false
-    el.visible = false
-    el.addEventListener('mouseenter', (): void => {
-      this._popupService.addMarkerPopup(id, feature)
-    })
-    el.addEventListener('mouseleave', (): void => {
-      this._popupService.removePopup()
-    })
-    return el
   }
 
   private createMarker(id: string, feature: Feature): Marker {
     const { OFFICE, PLACES, TRAILS } = this._layerElements
     const el: IHTMLMarkerElement = this.createHTMLMarkerElement(id, feature)
-
     const marker = (feature: Feature): Marker => {
       const { geometry } = feature
       return new Marker(el).setLngLat((geometry as Point).coordinates as LngLatLike)
@@ -86,7 +70,20 @@ export default class MarkerService {
       [PLACES, marker],
       [TRAILS, layerMarker]
     ])
-
     return markers.get(id)(feature)
+  }
+  /* create individual html marker elements & add mouse event handlers */
+  private createHTMLMarkerElement(id: string, feature: Feature): IHTMLMarkerElement {
+    const el: IHTMLMarkerElement = <IHTMLMarkerElement>document.createElement('div')
+    el.className = `${id}-marker`
+    el.isActive = false
+    el.isHidden = false
+    el.addEventListener('mouseenter', (): void => {
+      this._popupService.addMarkerPopup(id, feature)
+    })
+    el.addEventListener('mouseleave', (): void => {
+      this._popupService.removePopup()
+    })
+    return el
   }
 }
