4d63814aac52e86a385c9068a479fb0baa32eb4d
John Campbell <johncampbell@geospatialweb.ca>
2021-06-04 22:08:57 -0400

    refactor DataService

--- vue/src/services/data.ts
+++ vue/src/services/data.ts
@@ -1,6 +1,6 @@
 import * as fetch from 'd3-fetch'
 import mapboxgl from 'mapbox-gl'
-import { FeatureCollection } from 'geojson'
+import { Feature, FeatureCollection } from 'geojson'
 import { Container, Service } from 'typedi'
 
 import { markers, styleLayers } from '@/config'
@@ -10,39 +10,24 @@ import { HttpService, MarkerService, StyleLayerService } from '@/services'
 
 @Service()
 export default class DataService {
-  private _data: number[][] = []
   private _endPoints: Record<string, string> = EndPoints
+  private _hexagonLayerData: number[][] = []
   private _markers: IMarker[] = markers
   private _styleLayers: any[] = styleLayers
   private _urls: Record<string, string> = Urls
 
   constructor(
-    private _http: HttpService,
+    private _httpService: HttpService,
     private _markerService: MarkerService,
     private _styleLayerService: StyleLayerService
   ) {
-    this._http = Container.get(HttpService)
+    this._httpService = Container.get(HttpService)
     this._markerService = Container.get(MarkerService)
     this._styleLayerService = Container.get(StyleLayerService)
   }
 
   get hexagonLayerData(): number[][] {
-    return this._data
-  }
-
-  async getMapboxAccessToken(): Promise<void> {
-    try {
-      const { MAPBOX_ACCESS_TOKEN_ENDPOINT } = this._endPoints
-      /* prettier-ignore */
-      const { data: { token } } = await this._http.get(MAPBOX_ACCESS_TOKEN_ENDPOINT)
-
-      if (token && <Record<string, string>>token) {
-        return (mapboxgl.accessToken = token)
-      }
-      console.log(`No Mapbox Access Token Found:\n`, token)
-    } catch (err: any) {
-      console.error('Http Failed:\n', err)
-    }
+    return this._hexagonLayerData
   }
 
   loadData(): void {
@@ -50,18 +35,39 @@ export default class DataService {
     this.getMapLayerData()
   }
 
+  async getMapboxAccessToken(): Promise<void> {
+    try {
+      const { MAPBOX_ACCESS_TOKEN_ENDPOINT } = this._endPoints
+      /* prettier-ignore */
+      const { data: { token } } = await this._httpService.get(MAPBOX_ACCESS_TOKEN_ENDPOINT)
+      token && <string>token
+        ? this.setMapboxAccessToken(token)
+        : this.printConsoleLog(`No Mapbox Access Token Found:\n`, token)
+    } catch (err: any) {
+      this.printConsoleError(`${this.getMapboxAccessToken.name} Http Failed:\n`, err)
+    }
+  }
+
+  private setMapboxAccessToken(token: string): void {
+    mapboxgl.accessToken = token
+  }
+
   private getHexagonLayerData(): void {
-    const { HEXAGON_DATA_URL } = this._urls
+    const { HEXAGON_LAYER_DATA_URL } = this._urls
     fetch
-      .csv(HEXAGON_DATA_URL)
+      .csv(HEXAGON_LAYER_DATA_URL)
       .then((data: any[]): void => {
         data?.length
-          ? (this._data = data.map((d: Record<string, string>): number[] => [+d.lng, +d.lat]))
-          : console.error('Data Error:\n', data)
+          ? this.setHexagonLayerData(data)
+          : this.printConsoleError(`${this.getHexagonLayerData.name} Data Error:\n`, data)
       })
       .catch((err: Error): void => {
-        console.error('getHeatmapData Failed:\n', err)
+        this.printConsoleError(`${this.getHexagonLayerData.name} Fetch Failed:\n`, err)
       })
+  }
+
+  private setHexagonLayerData(data: any[]): void {
+    this._hexagonLayerData = data.map((d: Record<string, string>): number[] => [+d.lng, +d.lat])
   }
 
   private getMapLayerData(): void {
@@ -75,42 +81,52 @@ export default class DataService {
 
   private async getStyleLayers(styleLayer: IStyleLayer): Promise<void> {
     try {
-      /* prettier-ignore */
-      const { fields, layer, layer: { id }, table } = styleLayer
-      const fc = await this.getFeatureCollection({ fields, table })
-
-      if (fc?.features?.length) {
-        const styleLayer: any = layer
-        styleLayer.source.data = fc
-        return this._styleLayerService.setStyleLayers(styleLayer)
-      }
-      console.log(`No ${id.toUpperCase()} Layer Found:\n`, fc)
+      const { id } = styleLayer
+      const fc = await this.getGeoJsonFeatureCollection(styleLayer)
+      fc?.features?.length
+        ? this.setStyleLayers(fc, styleLayer)
+        : this.printConsoleLog(`No ${id.toUpperCase()} Layer Found:\n`, fc)
     } catch (err: any) {
-      console.error('Http Failed:\n', err)
+      this.printConsoleError(`${this.getStyleLayers.name} Http Failed:\n`, err)
     }
+  }
+
+  private setStyleLayers(fc: FeatureCollection, styleLayer: IStyleLayer): void {
+    styleLayer.source.data = fc
+    this._styleLayerService.setStyleLayers(styleLayer)
   }
 
   private async getMarkers(marker: IMarker): Promise<void> {
     try {
-      const { fields, table: id, table } = marker
-      const { features } = await this.getFeatureCollection({ fields, table })
-
-      if (features?.length) {
-        return this._markerService.setMarkers(id, features)
-      }
-      console.log(`No ${id.toUpperCase()} Markers Found:\n`, features)
+      const { id } = marker
+      const { features } = await this.getGeoJsonFeatureCollection(marker)
+      features?.length
+        ? this.setMarkers(id, features)
+        : this.printConsoleLog(`No ${id.toUpperCase()} Markers Found:\n`, features)
     } catch (err: any) {
-      console.error('Http Failed:\n', err)
+      this.printConsoleError(`${this.getMarkers.name} Http Failed:\n`, err)
     }
   }
 
-  private async getFeatureCollection({
-    fields,
-    table
+  private setMarkers(id: string, features: Feature[]): void {
+    this._markerService.setMarkers(id, features)
+  }
+
+  private async getGeoJsonFeatureCollection({
+    id,
+    fields
   }: IMarker | IStyleLayer): Promise<FeatureCollection> {
     const { GEOJSON_ENDPOINT } = this._endPoints
-    const params: IHttpParams = { fields, table }
-    const { data: fc } = await this._http.get(GEOJSON_ENDPOINT, { params })
+    const params: IHttpParams = { fields, table: id.replace(/-(.*)$/, '') }
+    const { data: fc } = await this._httpService.get(GEOJSON_ENDPOINT, { params })
     return fc
+  }
+
+  private printConsoleError(message: string, err: any): void {
+    console.error(message, err)
+  }
+
+  private printConsoleLog(message: string, data: any): void {
+    console.log(message, data)
   }
 }
