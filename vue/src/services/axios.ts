ec1974a3915e2af71b5002bd95bb780b50acdd45
John Campbell <johncampbell@geospatialweb.ca>
2021-06-06 11:21:04 -0400

    refactor AxiosService

--- vue/src/services/axios.ts
+++ vue/src/services/axios.ts
@@ -1,5 +1,19 @@
-import axios from 'axios'
+import axios, { AxiosInstance, AxiosStatic } from 'axios'
+import { Service } from 'typedi'
 
 import { Urls } from '@/enums'
 
-export default axios.create({ baseURL: Urls.API_BASE_URL })
+@Service()
+export default class AxiosService {
+  constructor(private _axios: AxiosStatic, private _instance: AxiosInstance) {
+    this._axios = axios
+  }
+
+  get instance(): AxiosInstance {
+    return this._instance
+  }
+
+  createInstance(): void {
+    this._instance = this._axios.create({ baseURL: Urls.API_BASE_URL })
+  }
+}
