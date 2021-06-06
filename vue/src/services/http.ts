d4ef091de7dc1c3575f68588f04c0f8f329b356b
John Campbell <johncampbell@geospatialweb.ca>
2021-06-06 11:21:55 -0400

    refactor HttpService

--- vue/src/services/http.ts
+++ vue/src/services/http.ts
@@ -1,11 +1,15 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios'
-import { Service } from 'typedi'
+import { Container, Service } from 'typedi'
 
 import { AxiosService } from '@/services'
 
 @Service()
 export default class HttpService {
+  constructor(private _axiosService: AxiosService) {
+    this._axiosService = Container.get(AxiosService)
+  }
+
   async get(url: string, params: AxiosRequestConfig = {}): Promise<AxiosResponse> {
-    return await AxiosService.get(url, params)
+    return await this._axiosService.instance.get(url, params)
   }
 }
