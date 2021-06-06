1f5a9514b661e16d4e6ac33a8774837cc897245f
John Campbell <johncampbell@geospatialweb.ca>
2021-06-06 11:19:52 -0400

    add mouse wheel zoom instruction

--- vue/src/components/Footer/index.tsx
+++ vue/src/components/Footer/index.tsx
@@ -2,8 +2,15 @@ import { defineComponent } from 'vue'
 
 import scss from './index.module.scss'
 
+const html = (): JSX.Element => (
+  <div class={scss.footer}>
+    <div>Use Mouse Wheel to zoom</div>
+    <div>Hold down Shift key to rotate map</div>
+  </div>
+)
+
 export default defineComponent({
   setup() {
-    return (): JSX.Element => <div class={scss.footer}>Hold down Shift key to rotate map</div>
+    return (): JSX.Element => html()
   }
 })
