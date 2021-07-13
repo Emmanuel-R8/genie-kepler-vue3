d348b5bf8225a730c9fbfd12d890428267697591
John Campbell <johncampbell@geospatialweb.ca>
2021-07-13 13:38:45 -0400

    delete scss references

--- vue/stylelint.config.js
+++ vue/stylelint.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   extends: [
-    'stylelint-config-recommended-scss',
+    'stylelint-config-recommended',
     'stylelint-config-css-modules',
     'stylelint-prettier/recommended'
   ],
-  rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true
-  }
+  rules: {}
 }
