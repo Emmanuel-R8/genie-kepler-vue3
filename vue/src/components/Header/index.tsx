b593c831683ff69664d51797ece5e310cb30efad
John Campbell <johncampbell@geospatialweb.ca>
2021-07-13 13:49:04 -0400

    change header title

--- vue/src/components/Header/index.tsx
+++ vue/src/components/Header/index.tsx
@@ -1,14 +1,18 @@
 import { defineComponent } from 'vue'
 
-import { header, name, title } from './index.module.scss'
+import { header, name, title } from './index.module.css'
 
 export default defineComponent({
   setup() {
     return (): JSX.Element => (
       <header class={header}>
         <img src="assets/logo.png" alt="Geospatial Web" />
-        <div class={name}>Geospatial Web</div>
-        <div class={title}>Go | Vue 3 Composition API | TSX | Mapbox GL | Deck.gl | PostGIS 3</div>
+        <h1 class={name}>Geospatial Web</h1>
+        <h2 class={title}>
+          Golang API&ensp;&#58;&ensp;Vue 3 Composition
+          API&ensp;&#58;&ensp;TSX&ensp;&#58;&ensp;Mapbox GL&ensp;&#58;&ensp;Deck
+          GL&ensp;&#58;&ensp;PostgreSQL
+        </h2>
         <a
           href="https://gitlab.com/geospatialweb/go-vue3-tsx"
           rel="noopener noreferrer"
