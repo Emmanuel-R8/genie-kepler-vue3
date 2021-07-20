036ddb2153f72bbd071608483cc6e1da5ad9c0aa
John Campbell <johncampbell@geospatialweb.ca>
2021-07-20 16:03:15 -0400

    add setBackgroundColor function

--- vue/src/components/Mapbox/index.tsx
+++ vue/src/components/Mapbox/index.tsx
@@ -1,7 +1,13 @@
 import { Container } from 'typedi'
 import { defineComponent, onBeforeUnmount, onMounted, onUnmounted } from 'vue'
 
-import { EventListenerService, MapService, MapboxService, MarkerService } from '@/services'
+import {
+  EventListenerService,
+  MapService,
+  MapboxService,
+  MapStyleService,
+  MarkerService
+} from '@/services'
 import { mapbox } from './index.module.css'
 
 const addEventListeners = (): void => {
@@ -18,6 +24,13 @@ const removeMapInstance = (): void => {
   const mapboxService = Container.get(MapboxService)
   mapboxService.map.remove()
 }
+const setBackgroundColor = (container: string): void => {
+  const el = document.querySelector(`#${container}`)
+  const mapStyleService = Container.get(MapStyleService)
+  const { mapStyle } = mapStyleService
+  /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
+  mapStyle.includes('outdoors') ? el!.classList.add('outdoors') : el!.classList.add('satellite')
+}
 const setMarkerVisibility = (): void => {
   const markerService = Container.get(MarkerService)
   markerService.setMarkerVisibility()
@@ -33,8 +46,9 @@ export default defineComponent({
   setup({ container }) {
     onMounted(async (): Promise<void> => {
       const mapService = Container.get(MapService)
-      await mapService.loadMapLayer()
       addEventListeners()
+      setBackgroundColor(container)
+      await mapService.loadMapLayer()
     })
     onBeforeUnmount((): void => {
       removeEventListeners()
